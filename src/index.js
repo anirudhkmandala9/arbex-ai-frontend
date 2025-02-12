ce3046766f718d6e504144e2da48b771f8075d2d
anirudhkmandala9 <akmandal@uci.edu>
2025-02-12 00:07:55 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure this path is correct
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
